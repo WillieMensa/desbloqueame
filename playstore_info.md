5555e576d431bdce8ea0c83cd11ae705792abdff
Willie <ingverger@gmail.com>
2019-12-21 23:19:33 -0300

    version 1.0.1
    version 1.0.1

---
+++ playstore_info.md
@@ -0,0 +1,7 @@
+Descripcion
+
+desbloqueame es un híbrido de Mastermind y el patrón de bloqueo de Android. Un juego que te encantará odiar.
+Esperemos que este juego (base de código incluida) te vuelva loco.
+El objetivo de este proyecto era descubrir aplicaciones web progresivas con trabajadores de servicios y jugar con Webpack. También para entretener porque el tubo es bastante aburrido, especialmente la línea central en la hora pico.
+Si te gusta este juego, debes convencer a 3 personas de que les guste, quienes tendrán que convencer a otras 3 personas ... y este proyecto se convertirá en la primera secta OSS. Además, si lo odias, debes convencer a 3 personas para que lo odien, quienes tendrán que convencer a otras 3 personas ...
+
---
+++ unlockme_lexicon.js
@@ -0,0 +1,34 @@
+// ----------------------------------------------------------------------------
+//	lexikon.json
+//	diccionario para version multilingual
+// ----------------------------------------------------------------------------
+
+const dictSp = {
+	txtDescrShort: "Híbrido de Mastermind y el patrón de bloqueo de Android. Un juego para amar y odiar.",
+	Enlaza los puntos para encontrar el patrón de bloqueo. Después de cada intento el juego te dirá cuantos puntos son correctos.
+	un punto está en el patrón en el orden correcto.
+	un punto está en el patrón pero en orden incorrecto.
+	Al establecer la dificultad cambia la cantidad de puntos a conectar. Facil: 4 puntos, medio: 5 puntos y dificil 6 puntos.
+	Buena suerte!_
+		by
+
+};
+
+
+const dictEn = {
+	txtDescrShort: "A hybrid of Mastermind and the Android pattern lock. A game you gonna love to hate.",
+	Link the dots to find the lock pattern. After every attempt the game will tell you how many dots you got right.
+	a dot occurs in the pattern and is in the correct order
+	a dot occurs in the pattern but in the wrong order
+	The difficulty setting changes the amount of dots to connect. Easy is 4 dots, medium is 5 dots and hard is 6 dots.
+	Good luck!_
+		by
+
+};
+
+
+
+const dictDe = {
+	txtDescrShort: "A hybrid of Mastermind and the Android pattern lock. A game you gonna love to hate.",
+};
+
